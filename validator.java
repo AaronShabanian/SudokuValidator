d83a87a7cbe4393ad906a1014ba7c8bfea232c9e
AaronShabanian <shabanian@chapman.edu>
2021-03-12 16:17:13 -0800

    Mostly done

--- validator.java
+++ validator.java
@@ -15,7 +15,7 @@ public class validator extends Thread {
         int[][] soduku = new int[9][9];
         //input parsing (converting file to 2d array)
         try{
-            File file = new File("tester.txt");
+            File file = new File("Testfile5.txt");
             Scanner reader = new Scanner(file);
             int counter=-1;
             while (reader.hasNextLine()){
@@ -67,11 +67,9 @@ public class validator extends Thread {
 
         //Comparing rows to columns
         for(int i=0; i<rowErrors.size(); i++){
-            System.out.println(rowErrors.get(i));
             for(int j=0; j<columnErrors.size(); j++){
-                System.out.println(columnErrors.get(j));
-                if((rowErrors.get(i)).equals(columnErrors.get(j))){
-                    if(!confirmedErrors.contains(rowErrors.get(i).substring(0,2))){
+                if((rowErrors.get(i)).equals(columnErrors.get(i))){
+                    if(!confirmedErrors.contains(rowErrors.get(i))){
                         confirmedErrors.add(rowErrors.get(i));
                     }
                 }
@@ -79,27 +77,27 @@ public class validator extends Thread {
         }
         for(int i=0; i<rowErrors.size(); i++){
             for(int j=0; j<boxErrors.size(); j++){
-                //System.out.println(boxErrors.get());
                 if((rowErrors.get(i)).equals(boxErrors.get(j))){
-                    if(!confirmedErrors.contains(rowErrors.get(i).substring(0,2))){
+                    if(!confirmedErrors.contains(rowErrors.get(i))){
                         confirmedErrors.add(rowErrors.get(i));
                     }
                 }
             }
         }
+        System.out.println(rowErrors);
+        System.out.println(columnErrors);
+        System.out.println(boxErrors);
         for(int i=0; i<columnErrors.size(); i++){
             for(int j=0; j<boxErrors.size(); j++){
                 if((columnErrors.get(i)).equals(boxErrors.get(j))){
-                    if(!confirmedErrors.contains(columnErrors.get(i).substring(0,2))){
+                    if(!confirmedErrors.contains(columnErrors.get(i))){
                         confirmedErrors.add(columnErrors.get(i));
                     }
                 }
             }
         }
         System.out.println("Confirmed Errors are");
-        for(int i =0; i<confirmedErrors.size(); i++){
-            System.out.println(confirmedErrors.get(i));
-        }
+        System.out.println(confirmedErrors);
     }
     
     //This function is called when an error is found and it wants to find which number is missing
@@ -122,19 +120,26 @@ public class validator extends Thread {
         String column="";
         String missingNum="";
         ArrayList <Integer> oneRow = new ArrayList(10);
+        ArrayList <String> onerowcoordinates = new ArrayList(10);
         for(int i=0; i<9; i++){
             oneRow.clear();
+            onerowcoordinates.clear();
             for(int j=0; j<9; j++){
                 if(oneRow.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
                     missingNum=Integer.toString(missingNumber(arr[i]));
-
                     //adds row and column of error to universal arraylist to be compared later
                     rowErrors.add(row+column+missingNum);
+                    for(int k=0; k<oneRow.size(); k++){
+                        if(oneRow.get(k)==arr[i][j]){
+                            rowErrors.add(onerowcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     oneRow.add(arr[i][j]);
+                    onerowcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
             }
         }
@@ -145,8 +150,10 @@ public class validator extends Thread {
     String missingNum="";
     int [] col=new int[9];
     ArrayList <Integer> oneColumn = new ArrayList(10);
+    ArrayList <String> onecolumncoordinates= new ArrayList<>(10);
         for(int i=0; i<9; i++){
             oneColumn.clear();
+            onecolumncoordinates.clear();
             for(int j=0; j<9; j++){
                 if(oneColumn.contains(arr[j][i])){
                     row=Integer.toString(j);
@@ -157,19 +164,28 @@ public class validator extends Thread {
                     missingNum=Integer.toString(missingNumber(col));
                     //adds row and column of error to universal arraylist to be compared later
                     columnErrors.add(row+column+missingNum);
+                    for(int k=0; k<oneColumn.size(); k++){
+                        if(oneColumn.get(k)==arr[j][i]){
+                            columnErrors.add(onecolumncoordinates.get(k)+missingNum);
+                        }
+                    }
+
                 }
                 else{
                     oneColumn.add(arr[j][i]);
+                    onecolumncoordinates.add(Integer.toString(j)+Integer.toString(i));
                 }
             }
         }
     }
     public static void boxes(int[][] arr){
         ArrayList <Integer> singleBox = new ArrayList(9);
+        ArrayList <String> singleboxcoordinates = new ArrayList<>(9);
         int boxnumtemp=1;
         String row="";
         String column="";
         String missingNum="";
+        int counter=0;
         int [] box=new int[9];
         //checks each box individually
         for(int i=0; i<3; i++){
@@ -177,184 +193,271 @@ public class validator extends Thread {
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=0; k<3; k++){
                         for(int l=0; l<3; l++){
-                            box[k*l]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
+        counter=0;
         boxnumtemp++;
         for(int i=0; i<3; i++){
             for(int j=3; j<6; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=0; k<3; k++){
                         for(int l=3; l<6; l++){
-                            box[k*(l-3)]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=0; i<3; i++){
             for(int j=6; j<9; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=0; k<3; k++){
                         for(int l=6; l<9; l++){
-                            box[k*(l-6)]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=3; i<6; i++){
             for(int j=0; j<3; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=3; k<6; k++){
                         for(int l=0; l<3; l++){
-                            box[(k-3)*l]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=3; i<6; i++){
             for(int j=3; j<6; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=3; k<6; k++){
                         for(int l=3; l<6; l++){
-                            box[(k-3)*(l-3)]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=3; i<6; i++){
             for(int j=6; j<9; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=3; k<6; k++){
                         for(int l=6; l<9; l++){
-                            box[(k-3)*(l-6)]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=6; i<9; i++){
             for(int j=0; j<3; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=6; k<9; k++){
                         for(int l=0; l<3; l++){
-                            box[(k-6)*l]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=6; i<9; i++){
             for(int j=3; j<6; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=6; k<9; k++){
                         for(int l=3; l<6; l++){
-                            box[(k-6)*(l-3)]=arr[k][l];
+                            box[counter]=arr[k][l];
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
         }
         singleBox.clear();
+        singleboxcoordinates.clear();
         boxnumtemp++;
+        counter=0;
         for(int i=6; i<9; i++){
             for(int j=6; j<9; j++){
                 if(singleBox.contains(arr[i][j])){
                     row=Integer.toString(i);
                     column=Integer.toString(j);
+                    counter=0;
                     for(int k=6; k<9; k++){
                         for(int l=6; l<9; l++){
-                            box[(k-6)*(l-6)]=arr[k][l];
+                            box[counter]=arr[k][l];
+                            counter++;
                         }
                     }
                     missingNum=Integer.toString(missingNumber(box));
-                    columnErrors.add(row+column+missingNum);
+                    boxErrors.add(row+column+missingNum);
+                    for(int k=0; k<singleBox.size(); k++){
+                        if(singleBox.get(k)==arr[i][j]){
+                            boxErrors.add(singleboxcoordinates.get(k)+missingNum);
+                        }
+                    }
                 }
                 else{
                     singleBox.add(arr[i][j]);
+                    singleboxcoordinates.add(Integer.toString(i)+Integer.toString(j));
                 }
                 
             }
